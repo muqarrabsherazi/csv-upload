b8a11ddf5c77d95d911dcbad37dfa79246f5242b
AbdullahTariqCS <AbdullahTariq.git@gmail.com>
2025-08-07 15:46:13 +0500

    added override utility

---
+++ packages/csv-upload/src/utils/override.ts
@@ -0,0 +1,26 @@
+import { FC, ReactNode } from "react";
+import { CSVCellCoords } from "types";
+
+
+export interface Override {
+  condition: (coords: CSVCellCoords) => boolean;
+  node: ReactNode | null;
+}
+
+export interface OverrideParams {
+  coords: CSVCellCoords;
+  baseNode: ReactNode;
+  condition: (coords: CSVCellCoords) => boolean;
+  overrides: Override[];
+}
+
+const override = ({ coords, baseNode, overrides, condition }: OverrideParams) => {
+  const node: ReactNode | null = overrides.reduce<ReactNode | null>(
+    (acc, ov) => ov.condition(coords) ? ov.node : acc,
+    null
+  );
+  return node ?? baseNode
+
+}
+
+export default override;
